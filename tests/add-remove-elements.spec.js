e3ec4bfc2a207db7b79fa9e56f0ce54f55dd4702
Manuel Abascal <manuelsuarezabascal@gmail.com>
2023-06-09 21:42:06 -0400

    added some comments for the code samples

--- tests/add-remove-elements.spec.js
+++ tests/add-remove-elements.spec.js
@@ -5,13 +5,16 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("user can add & remove elements", async ({ page }) => {
+    // gets the "Add Element" & clicks it
     const addButton = await page.getByRole('button', { name: 'Add Element' })
     await addButton.click();
 
+    // checks if the added button element is visible & has the text "Delete"
     const addedElement = await page.getByRole('button', { name: 'Delete' })
     await expect(addedElement).toBeVisible();
     await expect(addedElement).toHaveText("Delete");
 
+    // clicks the added button element & checks if it is not visible
     addedElement.click();
     await expect(addedElement).not.toBeVisible();
 });
