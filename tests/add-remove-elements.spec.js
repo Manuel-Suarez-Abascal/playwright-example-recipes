261fae9d0afe292c1377bfca86bef417266242b9
Manuel Abascal <manuelsuarezabascal@gmail.com>
2023-06-09 21:35:55 -0400

    added example code for add & removing elements

---
+++ tests/add-remove-elements.spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require("@playwright/test");
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+});
+
+test("user can add & remove elements", async ({ page }) => {
+    const addButton = await page.getByRole('button', { name: 'Add Element' })
+    await addButton.click();
+
+    const addedElement = await page.getByRole('button', { name: 'Delete' })
+    await expect(addedElement).toBeVisible();
+    await expect(addedElement).toHaveText("Delete");
+
+    addedElement.click();
+    await expect(addedElement).not.toBeVisible();
+});
